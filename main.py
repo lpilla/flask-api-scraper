e0fc2f76feb7329af317b5dc57cd2b93bc1005f3
lorenzo-vecchio <106695602+lorenzo-vecchio@users.noreply.github.com>
2023-04-15 19:16:15 +0200

    Update main.py
    aggiustamento id alunno e id corso anno

--- main.py
+++ main.py
@@ -23,8 +23,17 @@ def index():
         r.post(
             'https://itsar.registrodiclasse.it/geopcfp2/update/login.asp?1=1&ajax_target=DIVHidden&ajax_tipotarget=login',
             data=login_data)
+        req = r.get('https://itsar.registrodiclasse.it/geopcfp2/ajax/page/home.asp?1=1&ajax_target=DIVContenuto&ajax_tipotarget=home&z=1681492254027', cookies=r.cookies)
+        midSoup = BeautifulSoup(req.content, 'html.parser')
+        div = midSoup.find('div', class_='fullcalendar')
+        id_alunno = div['data-id-oggetto']
+        reqq = r.get('https://itsar.registrodiclasse.it/geopcfp2/ajax/page/voti_alunno.asp?1=1&ajax_target=DIVContenuto&ajax_tipotarget=voti_alunno&z=1681578054110', cookies=r.cookies)
+        midLateSoup = BeautifulSoup(reqq.content, 'html.parser')
+        input_tag = midLateSoup.find('input', {'id': 'LoadScriptDIVContenuto'})
+        value_attribute = input_tag['value']
+        iDCorsoAnno = value_attribute.split(',')[2].strip()
         requ = r.get(
-            'https://itsar.registrodiclasse.it/geopcfp2/ajax/page/componenti/voti_tabella.asp?idAlunno=2538&idMateria=&idCorsoAnno=210&id=&idUtenteCreazione=&ajax_target=DIV210_&ajax_tipotarget=undefined&z=1680172204309',
+            f'https://itsar.registrodiclasse.it/geopcfp2/ajax/page/componenti/voti_tabella.asp?idAlunno={id_alunno}&idMateria=&idCorsoAnno={iDCorsoAnno}&id=&idUtenteCreazione=&ajax_target=DIV210_&ajax_tipotarget=undefined&z=1680172204309',
             cookies=r.cookies)
     except:
         return jsonify({'error': 'could not access'}), 500
@@ -51,6 +60,10 @@ def index():
         return jsonify({'error': 'could not access'}), 500
     calendario = json.loads(cal)
     for event in calendario:
+        tooltip_info = event['tooltip'].split("<br>")
+        for info in tooltip_info:
+            if "Aula:" in info:
+                event['aula'] = info.split(": ")[1]
         del event['id']
         del event['borderColor']
         del event['backgroundColor']
